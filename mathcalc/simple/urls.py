b3426fe07b11be5af1fbb9a877df078ef5208253
TruePack <shnayder_a@outlook.com>
2019-01-14 01:41:14 +0700

    Refactored and commented out definitions, added answer to template

--- mathcalc/simple/urls.py
+++ mathcalc/simple/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calc),  # Home page call function which return form for entering number
-    path('simple/', views.search)  # return answer (prime factors)
+    path('', views.calc),  # Integer factorization
 ]
--- mathcalc/simple/views.py
+++ mathcalc/simple/views.py
@@ -1,37 +1,68 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def calc(request):
-    """Function of calling a page to enter a number."""
-    return render_to_response('simple/calc.html')
+def primfacs(number):
+    """Decomposes a number into prime factors.
 
+    number -- integer.
+    Returns a list of prime factors.
+    Between which there is a multiplication sign, as a list item.
 
-def primfacs(n):
-    """A function that decomposes the introduced number into prime factors.
-    Between factors, the multiplication sign.
     """
-    i = 2
-    primfac = []
-    if n == 1:
-        primfac.append(n)
+    min_prime_factor = 2  # Minimal prime factor
+    primfac = []  # Stores prime factors with multiplication signs.
+    if number == 1 or number == 0:  # Already prime factor
+        primfac.append(number)
         return primfac
-    while i * i <= n:
-        while n % i == 0:
-            primfac.append(i)
-            n = int(n / i)
-            primfac.append('*')  # Adding a sign in response for better readability of the answer
-        i = i + 1
-    if n > 1:
-        primfac.append(n)
-        primfac.append('*')
-    if len(primfac) > 1:  # Instruction is needed for a beautiful display without a sign * at the end (For ex. 2*2*2*)
+    while min_prime_factor * min_prime_factor <= number:
+        while number % min_prime_factor == 0:
+            primfac.append(min_prime_factor)
+            # Multiplication sign between prime factors.
+            primfac.append("*")
+            # Assign next branch for factorization cycle.
+            number = int(number / min_prime_factor)
+        # Try next factor
+        min_prime_factor = min_prime_factor + 1
+    # After cycles n must be prime factor.
+    if number > 1:
+        primfac.append(number)
+        primfac.append("*")
+    # Beautiful display without a sign * at the end (For ex. 2*2*2*).
+    if len(primfac) > 1:
         del primfac[-1]
     return primfac
 
 
-def search(request):
-    """The function of processing the entered number and returning the response."""
-    if 'q' in request.GET:
-        message = request.GET['q']
-    return HttpResponse(primfacs(int(message)))
+def calc(request):
+    """Call base template.
+
+    If hasn`t "number" in GET request will call page for enter number.
+    Else also call answer with prime factors.
+
+    """
+    if "number" in request.GET:
+        message = request.GET["number"]
+        int_message = int(message)
+        # For work definition primfacs(number) if number is negative.
+        if int_message < 0:
+            int_message *= -1
+            # Calculates prime factors. Convert list to string.
+            # And save negative sign for negative number.
+            list_of_primes = primfacs(int_message)
+            converted = map(str, list_of_primes)
+            answer = "-" + "".join(converted)
+        else:
+            # Calculates prime factors. Convert list to string.
+            list_of_primes = primfacs(int_message)
+            converted = map(str, list_of_primes)
+            answer = "".join(converted)
+        # For avoid explicit answer like 2=2, will return only number.
+        if len(list_of_primes) < 3:
+            message = ""
+        # Adds to prime factors equal sign.
+        else:
+            message = "= " + message
+        context = {"answer": answer, "message": message}
+        return render(request, "simple/calc.html", context)
+    else:
+        return render(None, "simple/calc.html")
